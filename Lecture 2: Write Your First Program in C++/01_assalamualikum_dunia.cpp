cd9b2ecf43a5612a8c20a239f2095aa0abdc40df
Hridoy <ridoyking9999@gmail.com>
2022-08-27 14:19:09 +0600

    Started DSA and Completed Lecture - 01 & Lecture 02

---
+++ Lecture 2: Write Your First Program in C++/01_assalamualikum_dunia.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    cout << "Assalamulaikum Dunia :-)" << endl;
+    // cout << "Assalamulaikum Dunia :-)" << '\n';
+    // cout << "Assalamulaikum Dunia :-)\n";
+    // cout << "Assalamulaikum Dunia :-)";
+    // cout << "\n";
+    return 0;
+}
